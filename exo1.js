8fe1bf5c71a90379083ff8aa74399266f251b7cc
Yekhlef <f.yekhlef@gmail.com>
2019-07-15 14:45:19 +0200

    First commit. Site avec premier test js

---
+++ exo1.js
@@ -0,0 +1,8 @@
+function on() {
+    document.querySelector("#ALERT").style.display = "block";
+    document.querySelector("#playalert").play();
+}
+  
+function off() {
+    document.querySelector("#ALERT").style.display = "none";
+}
